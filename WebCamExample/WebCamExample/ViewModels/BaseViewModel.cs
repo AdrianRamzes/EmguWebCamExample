8fca0b2a9c80e78264e41327cde6758fe4a22515
RmazesBlog <adrian@karalus.eu>
2015-04-08 14:10:22 +0200

    Init

---
+++ WebCamExample/WebCamExample/ViewModels/BaseViewModel.cs
@@ -0,0 +1,8 @@
+﻿using WebCamExample.Helpers;
+
+namespace WebCamExample.ViewModels
+{
+    public class BaseViewModel : NotificationObject
+    {
+    }
+}
---
+++ WebCamExample/WebCamExample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WebCamExample.Helpers;
+using WebCamExample.Services;
+
+namespace WebCamExample.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private WebCamService _webCamService;
+
+        private Bitmap _frame;
+        public Bitmap Frame
+        {
+            get
+            {
+                return _frame;
+            }
+
+            set
+            {
+                if (_frame != value)
+                {
+                    _frame = value;
+                    RaisePropertyChanged(() => Frame);
+                }
+            }
+        }
+
+        private ICommand _toggleWebServiceCommand;
+        public ICommand ToggleWebServiceCommand
+        {
+            get
+            {
+                return _toggleWebServiceCommand;
+            }
+
+            private set { }
+        }
+
+        public MainWindowViewModel()
+        {
+            InitializeServices();
+            InitializeCommands();
+        }
+
+        private void InitializeServices()
+        {
+            _webCamService = new WebCamService();
+            _webCamService.ImageChanged += _webCamService_ImageChanged;
+        }
+
+        private void InitializeCommands()
+        {
+            _toggleWebServiceCommand = new DelegateCommand(ToggleWebServiceExecute);
+        }
+
+        private void _webCamService_ImageChanged(object sender, System.Drawing.Bitmap image)
+        {
+            this.Frame = image;
+        }
+
+        private void ToggleWebServiceExecute()
+        {
+            if (!_webCamService.IsRunning)
+            {
+                _webCamService.RunServiceAsync();
+            }
+            else
+            {
+                _webCamService.CancelServiceAsync();
+            }
+        }
+    }
+}
